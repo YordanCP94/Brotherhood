9692d8ec8dc44b1b93cec6b56a45dc4c1858de5e
YordanCP94 <ycampospiedra@gmail.com>
2021-04-13 21:00:05 -0600

    Spring project initial structure was added.

---
+++ src/main/java/com/brotherhood/app/BrotherhoodApplication.java
@@ -0,0 +1,13 @@
+package com.brotherhood.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrotherhoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrotherhoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brotherhood/app/BrotherhoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brotherhood.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrotherhoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
